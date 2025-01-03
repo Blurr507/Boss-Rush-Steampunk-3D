4b6b3f387a2a8f432d7b9a246a01f4afbbd1fcf5
DeltaV <Tristantdm1@outlook.com>
2025-01-03 16:44:45 -0500

    testing
    testing once more

---
+++ Boss Rush Steampunk 3D/Assets/test.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ajajajaj
+		Debug.Log("It's alive!!!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
