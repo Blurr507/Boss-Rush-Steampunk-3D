1c463602336f23a0adea5e351c4d43c4dd1cf10d
pressgoal <isaiahmnasby@gmail.com>
2025-01-03 21:45:39 -0600

    Added health
    Added health

---
+++ Boss Rush Steampunk 3D/Assets/Scripts/Health.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+
+    public void AddHealth(int num)
+    {
+        health += num;
+    }
+
+    public void SubtractHealth(int num)
+    {
+        health -= num;
+    }
+}
