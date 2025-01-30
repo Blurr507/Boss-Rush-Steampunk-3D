b484b915e4e6a58b9b8da8f838ed32e9b4191048
pressgoal <isaiahmnasby@gmail.com>
2025-01-30 10:49:47 -0600

    Custom Cursor

---
+++ Boss Rush Steampunk 3D/Assets/Scripts/UI/CursorManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    public Texture2D cursorTexture;
+    public Texture2D cursorTextureHovering;
+    public Vector2 clickOffset = Vector2.zero;
+    public bool hovering = false;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        hovering = false;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+        {
+            hovering = hit.transform.GetComponent<CanSelect>() && hit.transform.GetComponent<CanSelect>().canSelect;
+        }
+        Cursor.SetCursor(hovering ? cursorTextureHovering : cursorTexture, clickOffset, CursorMode.Auto);
+    }
+}
