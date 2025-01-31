efd89958d97c6c94c1d89746da79b36d5fbfa733
pressgoal <isaiahmnasby@gmail.com>
2025-01-31 09:06:41 -0600

    Added music and bell to level transition

--- Boss Rush Steampunk 3D/Assets/Scripts/UI/LevelTransitionProgress.cs
+++ Boss Rush Steampunk 3D/Assets/Scripts/UI/LevelTransitionProgress.cs
@@ -3,8 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class LevelTransitionProgress : MonoBehaviour
 {
+    public AudioSource bellSound;
+
     public void NextScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PlayBell()
+    {
+        bellSound.Play();
     }
 }
